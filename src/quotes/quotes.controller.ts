6ed55bceb107dee6bf7bfa0d88c14c3f3e0e92f1
kosei <abdelhamid.boudjit@gmail.com>
2023-08-03 18:58:12 +0100

    added idValidation Pipe

--- src/quotes/quotes.controller.ts
+++ src/quotes/quotes.controller.ts
@@ -4,16 +4,18 @@ import {
   Post,
   Req,
   UseGuards,
+  UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { QuotesService } from './quotes.service';
-import { AddQuoteDTO, JwtAuthGuard } from 'src/common';
+import { AddQuoteDTO, IdValidationPipe, JwtAuthGuard } from 'src/common';
 
 @Controller('quotes')
 export class QuotesController {
   constructor(private readonly quotesService: QuotesService) {}
   @Post('add-quote')
   @UseGuards(JwtAuthGuard)
+  @UsePipes(IdValidationPipe)
   async addQuote(
     @Body(ValidationPipe) data: AddQuoteDTO,
     @Req() { userId }: { userId: string },
